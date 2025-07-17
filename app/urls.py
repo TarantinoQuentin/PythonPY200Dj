b28eb9635847a245faff74ba4c73428c4a3c266d
gridan <gregdenfv42@gmail.com>
2025-07-17 20:02:48 +0300

    Практика 17.07

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 from .views import template_view, index_view, login_view, register_view, \
     logout_view, user_detail_view, get_text_json
+from .views import TemplView, MyTemplView
+
 
 app_name = 'app'
 
 urlpatterns = [
     path('', index_view, name='index'),
-    path('template/', template_view, name='template'),
+    path('template/', MyTemplView.as_view(), name='template'),  # Пропишите path выше, чем прошлый, чтобы маршрутизатор
+    path('template/', TemplView.as_view(), name='template'),  # выполнял именно пример с классовым представлением,
+    path('template/', template_view, name='template'),  # а не функциональным (помним, что выполняется представление то, что выше при одинаковом маршруте).
     path('login/', login_view, name='login'),
     path('register/', register_view, name='register'),
     path('logout', logout_view, name='logout'),
--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.shortcuts import render
 from .models import get_random_text
 from django.http import HttpRequest, JsonResponse
@@ -5,6 +7,52 @@ from django.shortcuts import redirect
 from django.contrib.auth import login, logout, authenticate
 from .forms import TemplateForm, CustomUserCreationForm
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.views import View
+from django.views.generic import TemplateView
+
+
+class MyTemplView(TemplateView):
+    template_name = 'app/template_form.html'
+
+    def post(self, request, *args, **kwargs):
+        # прописываем всё что было в методе post в TemplView(View)
+        received_data = request.POST
+        form = TemplateForm(received_data)
+        if form.is_valid():
+            my_text = form.cleaned_data.get("my_text")
+            my_select = form.cleaned_data.get("my_select")
+            my_textarea = form.cleaned_data.get("my_textarea")
+            my_email = form.cleaned_data.get('my_email')
+            my_password = form.cleaned_data.get('my_password')
+            date = form.cleaned_data.get('date')
+            age = form.cleaned_data.get('age')
+            checkbox = form.cleaned_data.get('checkbox')
+            return JsonResponse(form.cleaned_data, json_dumps_params={'ensure_ascii': False})
+        context = self.get_context_data(**kwargs)  # Получаем контекст, если он есть
+        context["form"] = form  # Записываем в контекст форму
+        return self.render_to_response(context)  # Возвращаем вызов метода render_to_response
+
+
+class TemplView(View):
+    def get(self, request):
+        # скопируйте код, что есть в template_view в теле условия request.method == "GET"
+        return render(request, 'app/template_form.html')
+
+    def post(self, request):
+        # скопируйте код, что есть в template_view в теле условия request.method == "POST"
+        received_data = request.POST
+        form = TemplateForm(received_data)
+        if form.is_valid():
+            my_text = form.cleaned_data.get("my_text")
+            my_select = form.cleaned_data.get("my_select")
+            my_textarea = form.cleaned_data.get("my_textarea")
+            my_email = form.cleaned_data.get('my_email')
+            my_password = form.cleaned_data.get('my_password')
+            date = form.cleaned_data.get('date')
+            age = form.cleaned_data.get('age')
+            checkbox = form.cleaned_data.get('checkbox')
+            return JsonResponse(form.cleaned_data, json_dumps_params={'ensure_ascii': False})
+        return render(request, 'app/template_form.html', context={"form": form})
 
 
 def template_view(request):
@@ -22,19 +70,15 @@ def template_view(request):
             my_text = form.cleaned_data.get("my_text")  # Получили очищенные данные
             my_select = form.cleaned_data.get("my_select")
             my_textarea = form.cleaned_data.get("my_textarea")
+            # Получите остальные данные из формы и сделайте необходимые обработки (если они нужны)
             my_email = form.cleaned_data.get('my_email')
             my_password = form.cleaned_data.get('my_password')
             date = form.cleaned_data.get('date')
             age = form.cleaned_data.get('age')
             checkbox = form.cleaned_data.get('checkbox')
-
-            # Получите остальные данные из формы и сделайте необходимые обработки (если они нужны)
-
             # Верните HttpRequest или JsonResponse с данными
-
+            return JsonResponse(form.cleaned_data, json_dumps_params={'ensure_ascii': False})
         return render(request, 'app/template_form.html', context={"form": form})
-        # return JsonResponse(received_data, json_dumps_params={'ensure_ascii': False,
-        #                                              'indent': 4})
 
 
 def login_view(request):
